372b3689f467446e7f1b24faf0cba1a050085c20
keshavsharma25 <skeshav0825@gmail.com>
2026-08-31 13:19:57 +0530

    feat: Add SignatureKind::ExecutionProof

--- helper_functions/src/error.rs
+++ helper_functions/src/error.rs
@@ -107,6 +107,8 @@ pub enum SignatureKind {
     ExecutionPayloadBid,
     #[display("execution payload envelope signature")]
     ExecutionPayloadEnvelope,
+    #[display("execution proof signature")]
+    ExecutionProof,
     #[display("collection of multiple signatures")]
     Multi,
     #[display("payload attestation signature")]
